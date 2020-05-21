d86d552bf4689c33cb297ad7ab96f8e87bd41a7a
Lakshmi Chaitanya Rayala <lakshmi.rayala@agcocorp.com>
2020-05-21 08:46:01 -0500

    Added lambda dependency to package.json. Created a hello cdk application with lambda.

--- lib/aws-cdk-demo-stack.ts
+++ lib/aws-cdk-demo-stack.ts
@@ -1,18 +1,15 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class AwsCdkDemoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'AwsCdkDemoQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+        runtime: lambda.Runtime.NODEJS_10_X, //execution environment
+        code: lambda.Code.fromAsset('lambda'), //code loaded from "lambda" directory
+        handler: 'hello.handler' //file is "hello", function is "handler"
     });
-
-    const topic = new sns.Topic(this, 'AwsCdkDemoTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
